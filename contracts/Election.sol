1360c1e95073c92f901421bcbd8cfd180cf7fddf
RBaker <rbaker@vinsuite.com>
2019-02-21 06:34:01 -0800

    initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity >0.4.11;
+
+contract Election {
+    // Constructor
+    // Read candidate
+    // Constructor
+    string public candidate;
+
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
